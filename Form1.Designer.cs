f06ed0bfbcc9be6c5e04524a4f223240680fa8d9
giothegio <g.franceschini.05@gmail.com>
2020-11-17 12:56:33 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WA_BGW_Macchine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ptb_Car1 = new System.Windows.Forms.PictureBox();
+            this.ptb_Car2 = new System.Windows.Forms.PictureBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Car1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(151, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(386, 248);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ptb_Car1
+            // 
+            this.ptb_Car1.Image = ((System.Drawing.Image)(resources.GetObject("ptb_Car1.Image")));
+            this.ptb_Car1.Location = new System.Drawing.Point(151, 123);
+            this.ptb_Car1.Name = "ptb_Car1";
+            this.ptb_Car1.Size = new System.Drawing.Size(100, 50);
+            this.ptb_Car1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptb_Car1.TabIndex = 0;
+            this.ptb_Car1.TabStop = false;
+            // 
+            // ptb_Car2
+            // 
+            this.ptb_Car2.BackColor = System.Drawing.Color.Transparent;
+            this.ptb_Car2.Image = ((System.Drawing.Image)(resources.GetObject("ptb_Car2.Image")));
+            this.ptb_Car2.Location = new System.Drawing.Point(151, 243);
+            this.ptb_Car2.Name = "ptb_Car2";
+            this.ptb_Car2.Size = new System.Drawing.Size(100, 50);
+            this.ptb_Car2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptb_Car2.TabIndex = 0;
+            this.ptb_Car2.TabStop = false;
+            this.ptb_Car2.Click += new System.EventHandler(this.ptb_Car2_Click);
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(33, 31);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 1;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click_1);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(33, 88);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_Stop.TabIndex = 2;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(533, 88);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(112, 248);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.ptb_Car1);
+            this.Controls.Add(this.ptb_Car2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Car1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox ptb_Car1;
+        private System.Windows.Forms.PictureBox ptb_Car2;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WA_BGW_Macchine
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bgw1;
+        
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+      
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            bgw1.CancelAsync();
+            ptb_Car1.Location = new Point(151, 123);
+            ptb_Car2.Location = new Point(151, 243);
+            
+        }
+
+
+        private void Bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (ptb_Car1.Bounds.IntersectsWith(pictureBox2.Bounds) && !ptb_Car2.Bounds.IntersectsWith(pictureBox2.Bounds))
+            {
+                MessageBox.Show("Ha vinto la macchina 1");
+                
+            }
+            if (!ptb_Car1.Bounds.IntersectsWith(pictureBox2.Bounds) && ptb_Car2.Bounds.IntersectsWith(pictureBox2.Bounds))
+            {
+                MessageBox.Show("Ha vinto la macchina 2");
+            }
+        }
+
+        private void Bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Random rnd = new Random(Environment.TickCount);
+            if (!ptb_Car1.Bounds.IntersectsWith(pictureBox2.Bounds) && !ptb_Car2.Bounds.IntersectsWith(pictureBox2.Bounds))
+            {
+                ptb_Car1.Left += rnd.Next(3, 13);
+                ptb_Car2.Left += rnd.Next(3, 13);
+            }
+            else if ((ptb_Car1.Location.X == 534 || ptb_Car1.Location.Y == 123) || (ptb_Car2.Location.X == 534 || ptb_Car2.Location.Y == 243))
+            {
+                bgw1.CancelAsync();
+            }
+            
+            
+        }
+
+        private void Bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgw = sender as BackgroundWorker;
+
+            for (int i = 0; i < 500; i++)
+            {
+                Thread.Sleep(100);
+                bgw.ReportProgress(i);
+                if (bgw.CancellationPending)
+                {
+                    return;
+                }
+            }
+            
+        }
+
+        private void btn_Start_Click_1(object sender, EventArgs e)
+        {
+            bgw1 = new BackgroundWorker();
+            bgw1.WorkerReportsProgress = true;
+            bgw1.WorkerSupportsCancellation = true;
+            bgw1.DoWork += Bgw_DoWork;
+            bgw1.ProgressChanged += Bgw_ProgressChanged;
+            bgw1.RunWorkerCompleted += Bgw_RunWorkerCompleted;
+            bgw1.RunWorkerAsync();
+            
+
+        }
+
+        private void ptb_Car2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
